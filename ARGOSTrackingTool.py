a7ddaed771142bd181b93a2b96daab85dc29c38b
jozog13 <jessica.ozog@duke.edu>
2022-09-29 16:59:21 -0400

    Read a copy and pasted line of data

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,3 +9,18 @@
 # Date:   Fall 2022
 #--------------------------------------------------------------
 
+# Copy and paste one line of data as a variable
+lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+
+# Split lineString into a list of items
+lineData = lineString.split()
+
+# Assign variables to items in the lineData list
+record_id = lineData[0]   # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[4]       # Observation Location Class
+obs_lat = lineData[6]     # Observation Latitude
+obs_lon = lineData[7]     # Observation Longitude
+
+# Print information to the use
+print (f"Record {record_id} indicates Sara was seen at {obs_lat}N, {obs_lon}W on {obs_date}")
