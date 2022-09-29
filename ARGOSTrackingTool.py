497afa4360398d6f0f8c462da13d965b11f23f12
jozog13 <jessica.ozog@duke.edu>
2022-09-29 18:44:26 -0400

    created dictionaries

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,6 +21,10 @@ line_list = file_object.readlines()
 # Close the file object
 file_object.close()
 
+# Create empty dictionaries
+date_dict = {}
+location_dict = {}
+
 # Extract one data line into a variable
 for lineString in line_list:
 
@@ -37,6 +41,11 @@ for lineString in line_list:
     ob_lc = lineData[4]       # Observation Location Class
     obs_lat = lineData[6]     # Observation Latitude
     obs_lon = lineData[7]     # Observation Longitude
+    
+    # Add items to dictionaries
+    date_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
+    
     
     # Print information to the use
     print (f"Record {record_id} indicates Sara was seen at {obs_lat}N, {obs_lon}W on {obs_date}")
